d52685df733b0238ac6a32cf1a15ba4c338941a2
Quentin Harscoët <harscoet.quentin@gmail.com>
2020-12-01 17:47:06 +0100

    basic solution for part 2

--- 2020/day1/src/main.rs
+++ 2020/day1/src/main.rs
@@ -12,10 +12,22 @@ fn find_values(values: &Vec<i32>, target_value: i32) -> i32 {
     0
 }
 
+fn find_3_values(values: &Vec<i32>, target_value: i32) -> i32 {
+    for v in values{
+            let ret = find_values(values, target_value - v);
+            if ret != 0 {
+                return v * ret;
+            }
+        }
+    0
+}
+
+
 fn main() {
     let file = File::open("./src/input.txt").expect("Error opening the input");
     let buf_reader = BufReader::new(file);
     let values : Vec<i32> = buf_reader.lines().map(|line| line.unwrap().trim().parse().expect("Not a number") ).collect();
     println!("Hello, world! Contents is : \n{:?}", values);
     println!("Result : {}", find_values(&values, 2020));
+    println!("Result : {}", find_3_values(&values, 2020));
 }
