a21355f4cb19b39a3780664dd0bbead2eeefe9a1
Quentin Harscoët <harscoet.quentin@gmail.com>
2020-12-01 19:45:32 +0100

    more optimized solution

--- 2020/day1/src/main.rs
+++ 2020/day1/src/main.rs
@@ -1,5 +1,49 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::cmp::Ordering;
+
+// values need to be sorted, this is O(n) and implement
+fn find_pair_sorted(values: &[i32], target_value: i32) -> Option<i32> {
+
+    let mut l = 0;
+    let mut r = values.len() - 1;
+
+    while l < r {
+        match target_value.cmp(&(values[l] + values[r]))
+        {
+            Ordering::Less => r-=1,
+            Ordering::Greater => l += 1,
+            Ordering::Equal => return Some(values[l] * values[r]),
+        }
+    }
+    None
+}
+
+// values need to be sorted, this is O(n²)
+fn find_triplet_sorted(values: &[i32], target_value: i32) -> Option<i32> {
+    for (idx,val) in values.iter().enumerate() {
+        if let Some(ret) = find_pair_sorted(&values[idx..], target_value - val) {
+            return Some(val * ret);
+        }
+    }
+    None
+}
+
+fn find_pair(values: &Vec<i32>, target_value: i32) -> Option<i32> {
+    let mut values_copy =  values.to_vec();
+    values_copy.sort();
+    return find_pair_sorted(&values_copy, target_value);
+}
+
+fn find_triplet(values: &Vec<i32>, target_value: i32) -> Option<i32> {
+
+    let mut values_copy =  values.to_vec();
+    values_copy.sort();
+    return find_triplet_sorted(&values_copy, target_value);
+}
+
+
+// Naive solutions
 
 fn find_values(values: &Vec<i32>, target_value: i32) -> Option<i32> {
     for v in values {
@@ -21,6 +65,7 @@ fn find_3_values(values: &Vec<i32>, target_value: i32) -> Option<i32> {
     None
 }
 
+//Main
 fn main() {
     let file = File::open("./src/input.txt").expect("Error opening the input");
     let buf_reader = BufReader::new(file);
@@ -31,10 +76,10 @@ fn main() {
     println!("Hello, world! Contents is : \n{:?}", values);
     println!(
         "Result : {}",
-        find_values(&values, 2020).unwrap_or_default()
+        find_pair(&values, 2020).unwrap_or_default()
     );
     println!(
         "Result : {}",
-        find_3_values(&values, 2020).unwrap_or_default()
+        find_triplet(&values, 2020).unwrap_or_default()
     );
 }
