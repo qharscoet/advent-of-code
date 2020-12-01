19b10a7e7e3493bac11016dd67ca30fab11d28fa
Quentin Harscoët <harscoet.quentin@gmail.com>
2020-12-01 16:35:24 +0100

    Initial Commit with basic solution for day1-part1

---
+++ 2020/day1/src/main.rs
@@ -0,0 +1,21 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+fn find_values(values: &Vec<i32>, target_value: i32) -> i32 {
+    for v in values{
+        for v2 in values {
+            if  v + v2 == target_value {
+                return v * v2
+            }
+        }
+    }
+    0
+}
+
+fn main() {
+    let file = File::open("./src/input.txt").expect("Error opening the input");
+    let buf_reader = BufReader::new(file);
+    let values : Vec<i32> = buf_reader.lines().map(|line| line.unwrap().trim().parse().expect("Not a number") ).collect();
+    println!("Hello, world! Contents is : \n{:?}", values);
+    println!("Result : {}", find_values(&values, 2020));
+}
