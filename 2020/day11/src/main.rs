3f96ce62157584e9798c863765d4023cfdcd7e93
Quentin Harscoët <harscoet.quentin@gmail.com>
2020-12-11 22:32:56 +0100

    day11 part 2

--- 2020/day11/src/main.rs
+++ 2020/day11/src/main.rs
@@ -1,8 +1,49 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn step(grid: &mut Vec<Vec<char>>) -> bool
-{
+fn first_seat_occupied_dir(grid: &Vec<Vec<char>>, mut pos: (usize, usize), dir : (i32,i32) ) -> bool {
+    loop {
+        pos.0 = ((pos.0 as i32) +  dir.0) as usize;
+        pos.1 = ((pos.1 as i32) +  dir.1) as usize;
+
+        if !(0..grid.len()).contains(&pos.0) || !(0..grid[0].len()).contains(&pos.1){
+            return false;
+        }
+        else if grid[pos.0][pos.1] != '.' {
+            return  grid[pos.0][pos.1] == '#';
+        }
+    }
+}
+
+
+fn step_part2(grid: &mut Vec<Vec<char>>) -> bool {
+let mut new_grid :Vec<Vec<char>> = grid.clone();
+    let h = grid.len();
+    let w = grid[0].len();
+    let mut changed = false;
+
+    for i in 0..h {
+        for j in 0..w {
+            let dirs = vec![(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)];
+            let occupied_neigbours = dirs.iter().map(|d| first_seat_occupied_dir(&grid, (i,j), *d)).filter(|b| *b).count();
+
+            new_grid[i][j] = match grid[i][j] {
+                'L' => if occupied_neigbours == 0 {changed = true; '#'} else {'L'},
+                '#' => if occupied_neigbours >= 5 {changed = true; 'L'} else {'#'},
+                _ => {'.'}
+            }
+        }
+    }
+
+    for i in 0..h {
+        for j in 0..w {
+            grid[i][j] = new_grid[i][j];
+        }
+    }
+    changed
+}
+
+fn step(grid: &mut Vec<Vec<char>>) -> bool {
     let mut new_grid :Vec<Vec<char>> = grid.clone();
     let h = grid.len();
     let w = grid[0].len();
@@ -48,7 +89,7 @@ fn main() {
 
 
 
-    while step(&mut grid) {}
+    while step_part2(&mut grid) {}
     display(&grid);
     println!("Hello, world! Answer is : {:?}", grid.iter().flatten().filter(|&&c| c == '#').count());
 }
