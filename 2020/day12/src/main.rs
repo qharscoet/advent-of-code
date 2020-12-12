4f5a5075c7bc4c221920821fffba606d1d032882
Quentin Harscoët <harscoet.quentin@gmail.com>
2020-12-12 16:20:01 +0100

    day12 part 2

--- 2020/day12/src/main.rs
+++ 2020/day12/src/main.rs
@@ -38,8 +38,9 @@ impl std::str::FromStr for Action {
 }
 
 struct Ship{
-    pos:(i32,i32), // N/S, E/W
+    pos:(i32,i32), // E/W, N/S
     dir: u32, // [0-360]
+    waypoint: (i32,i32)
 }
 
 impl Ship {
@@ -47,20 +48,43 @@ impl Ship {
     const SIN_LUT: [i32;4] = [0, 1, 0, -1];
 
     fn new() -> Ship {
-        Ship { pos: (0,0), dir:0}
+        Ship { pos: (0,0), dir:0, waypoint: (10,1)}
     }
 
     fn process_action(&mut self, a: &Action){
         match a {
-            Action::North(v) => self.pos.0 += v,
-            Action::South(v) => self.pos.0 -= v,
-            Action::East(v) => self.pos.1 += v,
-            Action::West(v) => self.pos.1 -= v,
+            Action::North(v) => self.pos.1 += v,
+            Action::South(v) => self.pos.1 -= v,
+            Action::East(v) => self.pos.0 += v,
+            Action::West(v) => self.pos.0 -= v,
             Action::Left(v) => {self.dir += v; self.dir %= 360;}
             Action::Right(v) => { if v > &self.dir {self.dir += 360 - v;} else {self.dir -= v;}}
             Action::Forward(v) => {
                 let idx = (self.dir/90) as usize;
-                self.pos = (self.pos.0 + Ship::SIN_LUT[idx] * v, self.pos.1 + Ship::COS_LUT[idx] * v);
+                self.pos = (self.pos.0 + Ship::COS_LUT[idx] * v, self.pos.1 + Ship::SIN_LUT[idx] * v);
+            }
+        }
+    }
+
+    fn angle_rot(pos: (i32, i32), angle: u32) -> (i32, i32) {
+        let idx = (angle / 90) as usize;
+        (
+            pos.0 * Ship::COS_LUT[idx] - pos.1 * Ship::SIN_LUT[idx],
+            pos.0 * Ship::SIN_LUT[idx] + pos.1 * Ship::COS_LUT[idx],
+        )
+    }
+
+    fn process_action_part2(&mut self, a: &Action){
+        match a {
+            Action::North(v) => self.waypoint.1 += v,
+            Action::South(v) => self.waypoint.1 -= v,
+            Action::East(v) => self.waypoint.0 += v,
+            Action::West(v) => self.waypoint.0 -= v,
+            Action::Left(v) => self.waypoint = Ship::angle_rot(self.waypoint, *v),
+            Action::Right(v) => self.waypoint = Ship::angle_rot(self.waypoint, 360 - v),
+            Action::Forward(v) => {
+                self.pos.0 += self.waypoint.0 * v;
+                self.pos.1 += self.waypoint.1 * v;
             }
         }
     }
@@ -81,8 +105,10 @@ fn main() {
         .collect();
 
     let mut ship = Ship::new();
-
     actions.iter().for_each(|a| ship.process_action(a));
+    println!("Hello, world! Part 1 is {}", ship.manathan());
 
-    println!("Hello, world! {:?}", ship.manathan());
+    ship = Ship::new();
+    actions.iter().for_each(|a| ship.process_action_part2(a));
+    println!("Hello, world! Part 2 is {}", ship.manathan());
 }
