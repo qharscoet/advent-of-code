707ab3261f087f06d04c8e258cd055da53f09cdf
Quentin Harscoët <harscoet.quentin@gmail.com>
2020-12-15 19:21:28 +0100

    Day15 part 1

---
+++ 2020/day15/src/main.rs
@@ -0,0 +1,26 @@
+use std::collections::HashMap;
+
+fn main() {
+    const STARTING_NUMBERS : [u32; 7] =  [5, 2, 8, 16, 18, 0, 1];
+    const TURN_CNT: usize = 2020;
+
+    let mut age : HashMap<u32,u32> = HashMap::new();
+    let mut last_number = STARTING_NUMBERS[0];
+
+    for i in 1..TURN_CNT {
+        let number;
+        if i < STARTING_NUMBERS.len() {
+            number = STARTING_NUMBERS[i];
+        } else {
+            number = match age.get(&last_number) {
+                Some(last_spoken) => (i -1) as u32 - last_spoken,
+                None => 0,
+            };
+        }
+
+        age.insert(last_number, (i-1) as u32);
+        last_number = number;
+    }
+
+    println!("Hello, world! {}", last_number);
+}
