e5e771ec5bd5321761b8c8d6fe905a164b2a34e3
Quentin Harscoët <harscoet.quentin@gmail.com>
2022-12-01 14:40:51 +0100

    2022 day 1

--- aoc2022/src/solutions/day1.rs
+++ aoc2022/src/solutions/day1.rs
@@ -5,18 +5,25 @@ use crate::solution::Solution;
 pub struct Day1;
 
 impl Solution for Day1 {
-    type Input = Vec<i32>;
+    type Input = Vec<Vec<u32>>;
     type ReturnType = u32;
 
     fn parse_input(&self, lines: impl Iterator<Item = std::string::String>) -> Self::Input {
-        vec![1,2,3]
+        let v : Vec<Vec<u32>> = lines.fold(vec![vec![]], |mut acc, s| { match &s[..] {
+            "" => acc.push(vec![]),
+            _ => acc.last_mut().unwrap().push(s.parse().expect("Not a number !"))
+        } 
+        acc} );
+        v
     }
 
     fn first_part(&self, input: &Self::Input) -> u32 {
-        0
+        input.iter().map(|group| group.iter().sum()).max().unwrap_or_default()
     }
     fn second_part(&self, input: &Self::Input) -> u32 {
-        0
+        let mut sums : Vec<u32> = input.iter().map(|group| group.iter().sum()).collect();
+        sums.sort();
+        sums.iter().rev().take(3).sum()
     }
 }
 
@@ -28,16 +35,28 @@ mod tests {
     #[test]
     fn test_first_part() {
         assert_eq!(
-            Day1.first_part(&vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263]),
-            0
+            Day1.first_part(&vec![
+                vec![1000,2000,3000],
+                vec![4000],
+                vec![5000,6000],
+                vec![7000,8000,9000],
+                vec![10000]
+            ]),
+            24000
         );
     }
 
     #[test]
     fn test_second_part() {
         assert_eq!(
-            Day1.second_part(&vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263]),
-            0
+            Day1.second_part(&vec![
+                vec![1000,2000,3000],
+                vec![4000],
+                vec![5000,6000],
+                vec![7000,8000,9000],
+                vec![10000]
+            ]),
+            45000
         );
     }
 }
