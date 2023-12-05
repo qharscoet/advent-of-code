f4ca86306809c17a0876ab74a477d96bdedaf14b
Quentin Harscoët <harscoet.quentin@gmail.com>
2023-12-05 02:56:25 +0100

    day 3 p1

--- 2023/src/solutions/mod.rs
+++ 2023/src/solutions/mod.rs
@@ -1,5 +1,6 @@
 pub mod day1;
 pub mod day2;
+pub mod day3;
 
 use crate::solution::Solution;
 
@@ -7,6 +8,7 @@ pub fn run(day:u32) {
     match day {
         1 => { day1::Day1.solve(); }
         2 => { day2::Day2.solve(); }
+        3 => { day3::Day3.solve(); }
         _ => {}
     };
 }
