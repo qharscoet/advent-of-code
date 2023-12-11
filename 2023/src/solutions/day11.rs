b831e9ad24c88e39e4113b9c091d693155ea6ee5
Quentin Harscoët <harscoet.quentin@gmail.com>
2023-12-11 14:51:26 +0100

    day11 p2 (same)

--- 2023/src/solutions/day11.rs
+++ 2023/src/solutions/day11.rs
@@ -7,7 +7,7 @@ pub struct Day11;
 type Coord = (usize,usize);
 impl Solution for Day11 {
     type Input = HashSet<Coord>;
-    type ReturnType = u32;
+    type ReturnType = u64;
     const DAY : u32 = 11;
 
     fn parse_input(&self, lines: impl Iterator<Item = std::string::String>) -> Self::Input {
@@ -29,7 +29,6 @@ impl Solution for Day11 {
     }
 
     fn first_part(&self, input: &Self::Input) -> Self::ReturnType {
-        println!("{:?}", input);
         let prod : HashSet<_> = input.iter().flat_map(|p1| input.iter().map(move |p2| if p1 < p2 {(p1,p2)} else {(p2,p1)})).filter(|(p1,p2)| p1 != p2).collect();
         prod.iter().map(|(p1,p2)| {
             let diff_x : u32= (std::cmp::min(p1.0, p2.0)..std::cmp::max(p1.0,p2.0)).map(|x| if input.iter().any(|p| p.0 == x) {1u32} else {2u32}).sum();
@@ -37,10 +36,17 @@ impl Solution for Day11 {
             //println!("diff between {:?} and {:?} is {} + {} = {}", p1, p2, diff_x, diff_y, diff_x + diff_y );
 
             diff_x + diff_y
-        }).sum()
+        }).sum::<u32>() as u64
     }
-    fn second_part(&self, _input: &Self::Input) -> Self::ReturnType {
-        0
+    fn second_part(&self, input: &Self::Input) -> Self::ReturnType {
+        let prod : HashSet<_> = input.iter().flat_map(|p1| input.iter().map(move |p2| if p1 < p2 {(p1,p2)} else {(p2,p1)})).filter(|(p1,p2)| p1 != p2).collect();
+        prod.iter().map(|(p1,p2)| {
+            let diff_x : u64= (std::cmp::min(p1.0, p2.0)..std::cmp::max(p1.0,p2.0)).map(|x| if input.iter().any(|p| p.0 == x) {1u64} else {1000000u64}).sum();
+            let diff_y : u64= (std::cmp::min(p1.1, p2.1)..std::cmp::max(p1.1,p2.1)).map(|y| if input.iter().any(|p| p.1 == y) {1u64} else {1000000u64}).sum();
+            //println!("diff between {:?} and {:?} is {} + {} = {}", p1, p2, diff_x, diff_y, diff_x + diff_y );
+
+            diff_x + diff_y
+        }).sum()
     }
 }
 
@@ -60,8 +66,6 @@ mod tests {
 .......#..
 #...#.....";
 
-    static INPUT_TEST_2 : &str = "";
-
     #[test]
     fn test_first_part() {
         let lines = INPUT_TEST.split('\n').map(|s| s.to_string());
@@ -71,8 +75,8 @@ mod tests {
 
     #[test]
     fn test_second_part() {
-        let lines = INPUT_TEST_2.split('\n').map(|s| s.to_string());
+        let lines = INPUT_TEST.split('\n').map(|s| s.to_string());
         let input = Day11.parse_input(lines);
-        assert_eq!(Day11.second_part(&input), u32::MAX)
+        assert_eq!(Day11.second_part(&input), 8410)
     }
 }
