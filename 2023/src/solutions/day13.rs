7a025d20bf000ea25448cf1d052fe3ec0970c4a2
Quentin Harscoët <harscoet.quentin@gmail.com>
2023-12-14 12:58:49 +0100

    Fix day13 p2

--- 2023/src/solutions/day13.rs
+++ 2023/src/solutions/day13.rs
@@ -14,19 +14,34 @@ where
         .collect()
 }
 
-fn get_line(pat: &Pattern) -> Option<usize> {
+fn get_line(pat: &Pattern, filter: Option<usize>) -> Option<usize> {
     let len = pat[0].len();
-    (1..pat[0].len()).find(|&i| {
-        let range = if i <= len / 2 { i } else { len - i };
-
-        pat.iter().all(|line| {
-            let left: Vec<_> = line[(i - range)..i].iter().collect();
-            let right: Vec<_> = line[i..(i + range)].iter().rev().collect();
-
-            left == right
+    (1..pat[0].len())
+        .filter(|&i| match filter {
+            Some(v) => i != v,
+            None => true,
         })
-    })
+        .find(|&i| {
+            let range = if i <= len / 2 { i } else { len - i };
+
+            pat.iter().all(|line| {
+                let left: Vec<_> = line[(i - range)..i].iter().collect();
+                let right: Vec<_> = line[i..(i + range)].iter().rev().collect();
+
+                left == right
+            })
+        })
 }
+
+fn get_answer(pat: &Pattern, filter: Option<usize>) -> u32 {
+    if let Some(i) = get_line(&pat, filter) {
+        i as u32
+    } else {
+        100 * get_line(&transpose(&pat), filter.and_then(|f| Some(f / 100))).unwrap_or_default()
+            as u32
+    }
+}
+
 impl Solution for Day13 {
     type Input = Vec<Pattern>;
     type ReturnType = u32;
@@ -58,41 +73,20 @@ impl Solution for Day13 {
     }
 
     fn first_part(&self, input: &Self::Input) -> Self::ReturnType {
-        input
-            .iter()
-            .map(|pat| {
-                if let Some(i) = get_line(&pat) {
-                    i as u32
-                } else {
-                    100 * get_line(&transpose(&pat)).unwrap_or_default() as u32
-                }
-            })
-            .sum::<u32>()
+        input.iter().map(|pat| get_answer(&pat, None)).sum::<u32>()
     }
     fn second_part(&self, input: &Self::Input) -> Self::ReturnType {
         input
             .iter()
             .map(|pat| {
-                let old_line = if let Some(i) = get_line(&pat) {
-                    i as u32
-                } else {
-                    100 * get_line(&transpose(&pat)).unwrap_or_default() as u32
-                };
+                let old_line = get_answer(&pat, None);
 
                 let mut vals = (0..pat.len()).flat_map(|r| (0..pat[0].len()).map(move |c| (r, c)));
                 vals.find_map(|(r, c)| {
                     let mut pat_copy = pat.clone();
-                    pat_copy[r][c] = !pat_copy[r][c];
+                    pat_copy[r][c] = !pat[r][c];
 
-                    let line = if let Some(i) = get_line(&pat_copy) {
-                        if i as u32 != old_line {
-                            i as u32
-                        } else {
-                            100 * get_line(&transpose(&pat_copy)).unwrap_or_default() as u32
-                        }
-                    } else {
-                        100 * get_line(&transpose(&pat_copy)).unwrap_or_default() as u32
-                    };
+                    let line = get_answer(&pat_copy, Some(old_line as usize));
 
                     if line != old_line && line != 0 {
                         Some(line)
@@ -127,11 +121,26 @@ mod tests {
 ..##..###
 #....#..#";
 
+    static INPUT_ERROR: &str = "##.#.##
+##.#.##
+..#.#..
+.######
+#...#.#
+#.#####
+#.###.#";
+
     #[test]
     fn test_first_part() {
         let lines = INPUT_TEST.split('\n').map(|s| s.to_string());
         let input = Day13.parse_input(lines);
         assert_eq!(Day13.first_part(&input), 405)
+    }
+
+    #[test]
+    fn test_smudge() {
+        let lines = INPUT_ERROR.split('\n').map(|s| s.to_string());
+        let input = Day13.parse_input(lines);
+        assert_ne!(Day13.second_part(&input), 0)
     }
 
     #[test]
