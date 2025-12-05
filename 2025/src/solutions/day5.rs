46c2b253796d328f1417e2529c55fbe08267a9e4
Quentin Harscoët <harscoet.quentin@gmail.com>
2025-12-06 00:39:20 +0100

    day5 p2

--- 2025/src/solutions/day5.rs
+++ 2025/src/solutions/day5.rs
@@ -13,7 +13,7 @@ pub struct Ingredients {
 
 impl Solution for Day5 {
     type Input = Ingredients;
-    type ReturnType = usize;
+    type ReturnType = u64;
     const DAY : u32 = 5;
 
     fn parse_input(&self, mut lines: impl Iterator<Item = std::string::String>) -> Self::Input {
@@ -35,11 +35,49 @@ impl Solution for Day5 {
     fn first_part(&self, input: &Self::Input) -> Self::ReturnType {
         input.fresh_list.iter().filter(|&item| {
             input.ranges.iter().any(|range| range.contains(item))
-        }).count()
+        }).count() as u64
     }
     
     fn second_part(&self, input: &Self::Input) -> Self::ReturnType {
-        0
+        let mut ranges = input.ranges.clone();
+        ranges.sort_by(|a,b| {
+            if a.start() != b.start() {
+                a.start().cmp(b.start())
+            } else {
+                a.end().cmp(b.end())
+            }
+        });
+
+        let mut count = 0u64;
+        
+        let mut start = *ranges[0].start();
+        let mut end = *ranges[0].end();
+        for r in &ranges[1..] {
+            if end < *r.start() { //new disjointed range
+                count += end - start + 1; // +1 because inclusive
+                start = *r.start();
+                end = *r.end();
+            } else if end < *r.end() { //Overlap, only save the new end
+                end = *r.end();
+            }
+        }
+        
+        count += end - start +1; // last one
+        count
+        
+        /* Below does not work on input, don't know why, some range might be counted 2 times */
+        // let mut cursor = 0u64;
+        // for r in ranges {
+        //     if cursor < *r.start() { // new disjointed range, add its count
+        //         count += r.end() - r.start() +1; //+1 because range is inclusive
+        //     } else if cursor < *r.end() { // overlap, add the diff between the end of the previous and this one
+        //         count += r.end() - cursor; // No +1 as already counted the cursor pos
+        //     } else {
+        //         println!("noop");
+        //     }
+        //     cursor = *r.end();
+        //     println!("{:?} cursor {} count {}", r,  cursor, count);
+        // }
     }
 }
 
@@ -72,6 +110,6 @@ mod tests {
     fn test_second_part() {
         let lines = INPUT_TEST.split('\n').map(|s| s.to_string());
         let input = Day5.parse_input(lines);
-        assert_eq!(Day5.second_part(&input), 43);
+        assert_eq!(Day5.second_part(&input), 14);
     }
 }
